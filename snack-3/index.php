c7e00ba1cbd82759cf665393f7e6da6f2797f59a
SirAlyon <alessiosada04@gmail.com>
2022-05-20 15:25:44 +0200

    add snack 3

---
+++ snack-3/index.php
@@ -0,0 +1,15 @@
+
+<?php
+$array = [];
+var_dump(rand(1, 100));
+while (count($array) < 15){
+    $int = rand(1, 100);
+    if (!in_array($int, $array)){
+        array_push($array, $int);
+    }
+}
+var_dump($array)
+?>
+
+<!-- Creare un array con 15 numeri casuali, tenendo conto 
+che l’array non dovrà contenere lo stesso numero più di una volta -->
